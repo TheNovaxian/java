eeeb7521b121bc8895ae0356186db813b8506694
TheNovaxian <shaquille-36@hotmail.com>
2024-03-07 00:16:02 -0500

    Add files via upload

---
+++ Java/java.js
@@ -0,0 +1,15 @@
+function js_style()
+{
+   //font style adding by js
+   text.style.fontsize = "30px";
+   text.style.fontfamily = "Arial Bold";
+   text.style.color = "purple";
+   text.style.fontWeight = "bold";
+
+   text2.style.fontsize ="60px"
+   text2.style.fontFamily = "Cursive"
+   text2.style.color = "green"
+   text.style.fontWeight = "italic"
+   
+   
+}
